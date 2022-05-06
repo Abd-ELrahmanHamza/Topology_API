28c9bb6b4a5213d81c9d3178615c76f1f5f55400
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-06 15:52:27 +0200

    Main Class

--- src/API/MainClass.java
+++ src/API/MainClass.java
@@ -1,16 +1,87 @@
 package API;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MainClass {
+
+    private static Topologies topo = new Topologies();
+
     public static void main(String[] args) {
-        Topologies topo = new Topologies();
-        topo.readJSON("topology.json");
-        topo.readJSON("topology2.json");
+        while (true) {
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("1) Read a JSON file: ");
+            System.out.println("2) Write a JSON file: ");
+            System.out.println("3) Query Topologies: ");
+            System.out.println("4) Delete Topology: ");
+            System.out.println("5) Query Devices: ");
+            System.out.println("6) Query Devices With Netlist Node: ");
+            System.out.println("7) To exit ");
+            int choice = 0;
+            try {
+                choice = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input");
+            }
+            switch (choice) {
+                case 1 -> readJSONChoice();
+                case 2 -> writeJSONChoice();
+                case 3 -> QueryTopologiesChoice();
+                case 4 -> DeleteTopologyChoice();
+                case 5 -> QueryDevicesChoice();
+                case 6 -> QueryDevicesWithNetlistNodeChoice();
+                case 7 -> {
+                    return;
+                }
+                default -> System.out.println("Invalid input");
+            }
+        }
+    }
+
+    private static void readJSONChoice() {
+        System.out.printf("File Name: ");
+        Scanner in = new Scanner(System.in);
+        String fileName = in.next();
+        topo.readJSON(fileName);
+    }
+
+    private static void writeJSONChoice() {
+        System.out.printf("Topology Name: ");
+        Scanner in = new Scanner(System.in);
+        String topologyName = in.next();
+        System.out.println();
+        topo.writeJSON(topologyName);
+    }
+
+    private static void QueryTopologiesChoice() {
+        System.out.println("Existing topologies are :");
         System.out.println(topo.getTopologies());
-        System.out.println((topo.deleteTopology("top1")?"Deleted successfully":"Deleted unsuccessfully"));
-        topo.writeJSON("top2");
-        System.out.println(topo.getTopologies());
-        topo.readJSON("topology.json");
-        System.out.println(topo.queryDevices("top1"));
-        System.out.println(topo.queryDevicesWithNetlistNode("top2","n1"));
+    }
+
+    private static void DeleteTopologyChoice() {
+        System.out.printf("Topology Name: ");
+        Scanner in = new Scanner(System.in);
+        String topologyName = in.next();
+        System.out.println();
+        topo.deleteTopology(topologyName);
+    }
+
+    private static void QueryDevicesChoice() {
+        System.out.printf("Topology Name: ");
+        Scanner in = new Scanner(System.in);
+        String topologyName = in.next();
+        System.out.println();
+        System.out.println(topo.queryDevices(topologyName));
+    }
+
+    private static void QueryDevicesWithNetlistNodeChoice() {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Topology Name: ");
+        String topologyName = in.next();
+        System.out.printf("Node Name: ");
+        String deviceName = in.next();
+        System.out.println();
+        System.out.println(topo.queryDevicesWithNetlistNode(topologyName, deviceName));
     }
 }
