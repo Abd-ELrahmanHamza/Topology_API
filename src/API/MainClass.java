c0aaa4e653e05ad68933a6ae65623b09d3d251dd
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-20 20:30:52 +0200

    remove comments from mainClass

--- src/API/MainClass.java
+++ src/API/MainClass.java
@@ -37,16 +37,6 @@ public class MainClass {
                 default -> System.out.println("Invalid input");
             }
         }
-//        Topologies topo = new Topologies();
-//        topo.readJSON("topology.json");
-//        topo.readJSON("topology2.json");
-//        System.out.println(topo.getTopologies());
-//        System.out.println((topo.deleteTopology("top1") ? "Deleted successfully" : "Deleted unsuccessfully"));
-//        topo.writeJSON("top2");
-//        System.out.println(topo.getTopologies());
-//        topo.readJSON("topology.json");
-//        System.out.println(topo.queryDevices("top1"));
-//        System.out.println(topo.queryDevicesWithNetlistNode("top2", "n1"));
     }
 
     private static void readJSONChoice() {
